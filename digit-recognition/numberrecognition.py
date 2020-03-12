15b87868a5a16ddbaf2073f493534ee3aa197e08
Alexandre Salem <me@alexandresalem.com>
2020-03-12 14:45:49 -0500

    Adding Digit Recognition

--- digit-recognition/numberrecognition.py
+++ digit-recognition/numberrecognition.py
@@ -5,31 +5,28 @@ import matplotlib.pyplot as plt
 from keras.utils import to_categorical
 from keras.constraints import maxnorm
 from keras.preprocessing.image import ImageDataGenerator
-import tensorflow as tf
 from keras.callbacks import TensorBoard
 
-# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
-# sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
-turn = 7
+# Reading Training Data
 train_data = pd.read_csv('train.csv')
+
+# Separating Features form Labels
 train_features = train_data.iloc[:,1:]
 train_labels = train_data['label']
 
-tamanho = len(train_data)
 
-import time
+
+# Vectorizing Data
 train_features = train_features.to_numpy()
-train_features = np.reshape(train_features, (tamanho,28,28,1))
+size = len(train_data)
+train_features = np.reshape(train_features, (size,28,28,1))
 train_features = train_features.astype('float32')/255
 train_labels = train_labels.to_numpy().astype('float32')
 train_labels = to_categorical(train_labels)
 
-name = f'Digit-Recognition-CNN-128x2-256-128-{int(time.time())}'
-tensorboard = TensorBoard(log_dir=f'logs/{name}')
-
+# Creating the model
 def build_model():
-
     model = models.Sequential()
     model.add(layers.Conv2D(128,(3,3),activation='relu',input_shape=(28,28,1), kernel_constraint=maxnorm(3)))
     model.add(layers.MaxPooling2D(2,2))
@@ -38,18 +35,16 @@ def build_model():
     model.add(layers.Conv2D(256, (3, 3),kernel_constraint=maxnorm(3)))
     model.add(layers.MaxPooling2D(2, 2))
     model.add(layers.Flatten())
-
     model.add(layers.Dense(128,activation='relu',kernel_constraint=maxnorm(3)))
     model.add(layers.Dropout(0.4))
     model.add(layers.Dense(10, activation='softmax'))
     model.compile(optimizer=optimizers.RMSprop(), loss='categorical_crossentropy', metrics=['acc'])
-
     return model
 
 
-
+# Making K-Fold Validation
 k = 5
-num_val_samples = tamanho//k
+num_val_samples = size//k
 num_epochs = 5
 all_scores = []
 
@@ -64,46 +59,51 @@ for i in range(k):
         train_labels[:i*num_val_samples],
         train_labels[(i + 1) * num_val_samples:]],axis=0)
     model = build_model()
-    history = model.fit(partial_train_features,partial_train_labels,epochs=num_epochs,batch_size=32, validation_data=(val_features,val_labels), callbacks=[tensorboard])
+    history = model.fit(partial_train_features,partial_train_labels,epochs=num_epochs,batch_size=32, validation_data=(val_features,val_labels))
 
 
-
+# Reading Test Data and Vectorizing
 test_data = pd.read_csv('test.csv')
 test_features = test_data.to_numpy()
 preview_features = np.reshape(test_features,(len(test_data),28,28))
 test_features = np.reshape(test_features, (len(test_data),28,28,1))
 
-# plt.imshow(preview_features[3])
-# plt.title('0 ou 9')
+# Plot any image using Matplotlib Pyplot
+# test_row = 3 # You can select any number from 0 to len(test_data)
+# plt.imshow(preview_features[test_row])
 # plt.show()
 
 test_features = test_features.astype('float32')/255
 
+
+# Running the model
 model = build_model()
 history = model.fit(train_features,train_labels,epochs=10,verbose=1,batch_size=128)
 
 
-
-# serialize model to JSON
+# Saving model to JSON
+turn = 1 #To differentiate your models names
 model_json = model.to_json()
 with open(f"model{turn}.json", "w") as json_file:
     json_file.write(model_json)
-# serialize weights to HDF5
 model.save_weights(f"model{turn}.h5")
-
 print("Saved model to disk")
+
 
 # load json and create model
 json_file = open(f'model{turn}.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = models.model_from_json(loaded_model_json)
-# load weights into new model
 loaded_model.load_weights(f"model{turn}.h5")
 print("Loaded model from disk")
 
 
+# Predicting Test Data
 results = loaded_model.predict(test_features)
+
+
+# Saving predictions into csv file
 numbers = []
 index = []
 i = 1
@@ -117,8 +117,4 @@ for result in results:
 df = pd.DataFrame()
 df['ImageId'] = index
 df['Label'] = numbers
-
 df.to_csv(r'submission7.csv',index=False,header=True)
-
-
-
